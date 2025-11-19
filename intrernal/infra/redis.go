bff340cd3f559cedbab11a37d9b302f7d7ecc03e
moji <m.rakhis@digikala.com>
2025-11-19 15:48:34 +0330

    feat: add redis

---
+++ intrernal/infra/redis.go
@@ -0,0 +1,26 @@
+package infra
+
+import (
+	"arvan/message-gateway/intrernal/config"
+	"context"
+	"fmt"
+	log "github.com/sirupsen/logrus"
+
+	"github.com/redis/go-redis/v9"
+)
+
+func NewRedisClient(ctx context.Context, cfg config.Redis, logger *log.Logger) (*redis.Client, error) {
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     fmt.Sprintf("%s:%s", cfg.Host, cfg.Port),
+		Password: cfg.Password,
+		DB:       cfg.Database,
+	})
+
+	_, err := rdb.Ping(ctx).Result()
+	logger.Info(fmt.Sprintf("redis is running on %s : %s on db %d", cfg.Host, cfg.Port, cfg.Database))
+	if err != nil {
+		return nil, err
+	}
+
+	return rdb, err
+}
