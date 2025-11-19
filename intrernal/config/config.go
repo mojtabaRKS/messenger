8441d878238189cf36abf51f49743e509cf7bd5c
moji <m.rakhis@digikala.com>
2025-11-19 15:50:20 +0330

    feat: add config

---
+++ intrernal/config/config.go
@@ -0,0 +1,46 @@
+package config
+
+import "github.com/sirupsen/logrus"
+
+type AppEnv string
+
+const (
+	ProductionEnv AppEnv = "production"
+	StageEnv      AppEnv = "stage"
+	DevelopEnv    AppEnv = "develop"
+	LocalEnv      AppEnv = "local"
+	TestEnv       AppEnv = "test"
+)
+
+type (
+	Config struct {
+		AppEnv   AppEnv
+		LogLevel logrus.Level
+		HTTP     HTTP
+		Database Database
+	}
+
+	HTTP struct {
+		Port int
+	}
+
+	Database struct {
+		Postgres Postgres
+		Redis    Redis
+	}
+
+	Postgres struct {
+		Host     string
+		Port     int
+		Username string
+		Password string
+		Database string
+	}
+
+	Redis struct {
+		Host     string
+		Port     int
+		Password string
+		Database int
+	}
+)
---
+++ intrernal/config/reader.go
@@ -0,0 +1,52 @@
+package config
+
+import (
+	"errors"
+	"fmt"
+
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/viper"
+)
+
+func Load() (*Config, error) {
+
+	viper.SetConfigType("env")
+	viper.AddConfigPath(".")
+	viper.SetConfigName(".env")
+	viper.AllowEmptyEnv(true)
+	viper.SetDefault("APP_ENV", LocalEnv)
+	viper.AutomaticEnv()
+	if err := viper.ReadInConfig(); err != nil {
+		if !errors.As(err, &viper.ConfigFileNotFoundError{}) {
+			return nil, fmt.Errorf("reading config: %w", err)
+		}
+	}
+
+	logLvl, err := logrus.ParseLevel(viper.GetString("LOG_LEVEL"))
+	if err != nil {
+		return nil, fmt.Errorf("parsing LOG_LEVEL: %w", err)
+	}
+
+	return &Config{
+		AppEnv:   AppEnv(viper.GetString("APP_ENV")),
+		LogLevel: logLvl,
+		HTTP: HTTP{
+			Port: viper.GetInt("HTTP_PORT"),
+		},
+		Database: Database{
+			Postgres: Postgres{
+				Host:     viper.GetString("POSTGRES_HOST"),
+				Port:     viper.GetInt("POSTGRES_PORT"),
+				Username: viper.GetString("POSTGRES_USER"),
+				Password: viper.GetString("POSTGRES_PASSWORD"),
+				Database: viper.GetString("POSTGRES_DB"),
+			},
+			Redis: Redis{
+				Host:     viper.GetString("REDIS_HOST"),
+				Port:     viper.GetInt("REDIS_PORT"),
+				Password: viper.GetString("REDIS_PASSWORD"),
+				Database: viper.GetInt("REDIS_DB"),
+			},
+		},
+	}, nil
+}
