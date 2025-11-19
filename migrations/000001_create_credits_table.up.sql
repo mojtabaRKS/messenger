5cbd53dbc93ba2c61ef2dc29d11208c1e92c8234
moji <m.rakhis@digikala.com>
2025-11-19 18:16:38 +0330

    feat: add created_at to credits

--- migrations/000001_create_credits_table.up.sql
+++ migrations/000001_create_credits_table.up.sql
@@ -3,5 +3,6 @@ CREATE TABLE credits
     id              BIGSERIAL PRIMARY KEY,
     user_id         TEXT UNIQUE NOT NULL,
     credit_balance  BIGINT NOT NULL DEFAULT 0,
+    created_at      TIMESTAMPTZ NOT NULL DEFAULT now()
     updated_at      TIMESTAMPTZ NOT NULL DEFAULT now()
 );
