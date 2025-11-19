c1010e3413243fecf120a41908b7cfd6ef7fd320
moji <m.rakhis@digikala.com>
2025-11-19 15:48:21 +0330

    feat: add command files

---
+++ cmd/command/server.go
@@ -0,0 +1,61 @@
+package command
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/pkg/errors"
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/cobra"
+
+	"arvan/message-gateway/intrernal/api"
+	"arvan/message-gateway/intrernal/api/handler/sms"
+	"arvan/message-gateway/intrernal/config"
+	"arvan/message-gateway/intrernal/infra"
+)
+
+type Server struct {
+	Logger *logrus.Logger
+}
+
+func (cmd Server) Command(ctx context.Context, cfg *config.Config) *cobra.Command {
+	return &cobra.Command{
+		Use:   "server",
+		Short: "run Gateway server",
+		Run: func(_ *cobra.Command, _ []string) {
+			cmd.main(cfg, ctx)
+		},
+	}
+}
+
+func (cmd Server) main(cfg *config.Config, ctx context.Context) {
+	_, err := infra.NewPostgresClient(ctx, cfg.Database.Postgres)
+	if err != nil {
+		cmd.Logger.WithContext(ctx).Fatal(errors.Wrap(err, "server : failed to connect to postgresql"))
+		return
+	}
+
+	redisClient, err := infra.NewRedisClient(ctx, cfg.Database.Redis, cmd.Logger)
+	if err != nil {
+		cmd.Logger.WithContext(ctx).Fatal(errors.Wrap(err, "server : failed to connect to redis"))
+		return
+	}
+
+	defer func() {
+		if err = redisClient.Close(); err != nil {
+			cmd.Logger.WithContext(ctx).Fatal(errors.Wrap(err, "server : failed to close redis"))
+		}
+	}()
+
+	smsHandler := sms.New()
+
+	server := api.New(cfg.AppEnv)
+	server.SetupAPIRoutes(
+		smsHandler,
+	)
+
+	// run the server
+	if err := server.Serve(ctx, fmt.Sprintf(":%d", cfg.HTTP.Port)); err != nil {
+		cmd.Logger.Fatal(err)
+	}
+}
---
+++ cmd/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"arvan/message-gateway/cmd/command"
+	"arvan/message-gateway/intrernal/config"
+	"context"
+	"os/signal"
+	"syscall"
+
+	log "github.com/sirupsen/logrus"
+	"github.com/spf13/cobra"
+)
+
+func main() {
+	ctx, cancel := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM, syscall.SIGKILL)
+	defer cancel()
+
+	const description = "Message Gateway Server"
+	root := &cobra.Command{Short: description}
+
+	cfg, err := config.Load()
+	if err != nil {
+		log.WithContext(ctx).Fatal(err)
+	}
+
+	logger := log.New()
+
+	root.AddCommand(
+		command.Server{Logger: logger}.Command(ctx, cfg),
+		command.MigrateCommand{Logger: logger}.Command(ctx, cfg),
+	)
+
+	if err := root.Execute(); err != nil {
+		logger.WithContext(ctx).Fatalf("failed to execute root command: \n%v", err)
+	}
+}
