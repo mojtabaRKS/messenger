a3a954784c2277e5bb9a2e650bc92021c8015c60
moji <m.rakhis@digikala.com>
2025-11-21 17:44:38 +0330

    feat: add kafka reader

--- internal/infra/kafka.go
+++ internal/infra/kafka.go
@@ -11,11 +11,23 @@ import (
 func NewKafkaWriter(cfg config.Kafka) *kafka.Writer {
 	return &kafka.Writer{
 		Addr:         kafka.TCP(fmt.Sprintf("%s:%d", cfg.Host, cfg.Port)),
-		Topic:        constant.KafkaTopic,
+		Topic:        constant.TopicAccepted,
 		Balancer:     &kafka.Hash{},
 		RequiredAcks: constant.KafkaProducerAcks,
 		Async:        false, // workers perform sync writes with timeout + retries
 		BatchTimeout: 10 * time.Millisecond,
 		BatchSize:    1024,
 	}
+}
+
+func NewKafkaConsumer(cfg config.Kafka) *kafka.Reader {
+	return kafka.NewReader(kafka.ReaderConfig{
+		Brokers:               []string{fmt.Sprintf("%s:%d", cfg.Host, cfg.Port)},
+		Topic:                 constant.TopicAccepted,
+		GroupID:               constant.KafkaGroupID,
+		MinBytes:              1e3,  // 1KB
+		MaxBytes:              10e6, // 10MB
+		MaxWait:               500 * time.Millisecond,
+		WatchPartitionChanges: true,
+	})
 }
