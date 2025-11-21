15e12e20409a087cf98569ea6d9d3543b1e0822d
moji <m.rakhis@digikala.com>
2025-11-21 19:22:02 +0330

    fix: uncomment provide failures mock

--- internal/provider/stub.go
+++ internal/provider/stub.go
@@ -3,23 +3,25 @@ package provider
 import (
 	"arvan/message-gateway/internal/domain"
 	"fmt"
+	"math/rand"
+	"time"
 )
 
 func (s *StubProvider) Send(job domain.Job) error {
 	// Simulate latency 50-200ms
-	//lat := 50 + rand.Intn(151)
-	//time.Sleep(time.Duration(lat) * time.Millisecond)
+	lat := 50 + rand.Intn(151)
+	time.Sleep(time.Duration(lat) * time.Millisecond)
 
 	// 10% failure
-	//if rand.Float32() < 0.1 {
-	//	return fmt.Errorf("provider: simulated failure for job %s", job.ID)
-	//}
-	//
-	//// 20% proccess may take longer than usual
-	//if rand.Float32() < 0.2 {
-	//	time.Sleep(time.Duration(rand.Intn(10)) * time.Second)
-	//	return nil
-	//}
+	if rand.Float32() < 0.1 {
+		return fmt.Errorf("provider: simulated failure for job %s", job.ID)
+	}
+
+	// 20% process may take longer than usual
+	if rand.Float32() < 0.2 {
+		time.Sleep(time.Duration(rand.Intn(10)) * time.Second)
+		return nil
+	}
 
 	// print success message
 	fmt.Print("Sms sent successfully")
